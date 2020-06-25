f4b7890f0cfd544ab8ea7ea01ce3b27621e642e1
zacwoll <zacwoll@gmail.com>
2020-06-25 22:08:01 +0000

    split off test code into main

--- models/base_model.py
+++ models/base_model.py
@@ -43,17 +43,3 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
-
-    def 
-""" ========================== """
-
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
