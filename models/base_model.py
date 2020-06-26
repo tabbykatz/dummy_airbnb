7cd8087125d99383f221cee57741c69014a53cb7
zacwoll <zacwoll@gmail.com>
2020-06-26 23:24:50 +0000

    Magic Demon Bug go Bye-Bye

--- models/base_model.py
+++ models/base_model.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
-""" Base Class """
+""" Base Class Module """
+
 import uuid
 from datetime import datetime
 from models import storage
-
 
 tf = "%Y-%m-%dT%H:%M:%S.%f" #time format
 
 class BaseModel:
     """ Base Class """
 
-    def __init__(self, **kwargs):
-        """ init a Base Instance """
+    def __init__(self, *args, **kwargs):
+        """ init a Base instance """
 
-        if kwargs:
+        if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
                         kwargs["created_at"], tf)
-                if key == 'updated_at':
+                elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
                         kwargs["updated_at"], tf)
                 else:
@@ -30,19 +30,20 @@ class BaseModel:
             storage.new(self)
 
     def __str__(self):
-        """ Print a Base Model"""
-        return "[{}] ({}) {}".format(type(self).__name__,
-                                     self.id, self.__dict__)
+        """ Print a BaseModel() """
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Updates the public instance attribute 'updated_at' with the
-        current datetime """
+        """ Updates the public instance attribute 'updated_at' with the current
+        datetime """
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
-        """ Returns a dictionary containing all keys/values of __dict__
-        of the instance """
+        """Returns a dictionary containing all key/values of __dict__ of the
+        instance """
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
