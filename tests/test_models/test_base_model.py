1466fdaf34f77677315254130bc7445a4a33262a
tabbykatz <tomelay@gmail.com>
2020-06-26 01:42:07 +0000

    more fab tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -33,6 +33,25 @@ class TestBaseModel(unittest.TestCase):
         msg = "__init__() missing 1 required positional argument: 'self'"
         self.assertEqual(str(e.exception), msg)
 
+    def test_init_such_args(self):
+        """such args. much wow."""
+        pass
+
+    def test_datetime_creation(self):
+        """checking for datetime function"""
+        rat = BaseModel()
+        date = datetime.now()
+        difference = rat.updated_at - rat.created_at
+        self.assertTrue(abs(difference.total_seconds()) < 0.01)
+        difference = rat.created_at - date
+        self.assertTrue(abs(difference.total_seconds()) < 0.1)
+
+    def test_ids_special(self):
+        """test for unique ids"""
+        rat = [BaseModel().id for i in range(1000)]
+        self.assertEqual(len(set(rat)), len(rat))
+
+
 
 if __name__ == '__main__':
     unittest.main()
