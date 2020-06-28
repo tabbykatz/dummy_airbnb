4eb1e00e7b1d3d4cb4cd1ee32e8f2d043ac988ab
Cynthia Taylor <820@holbertonschool.com>
2020-06-28 00:50:51 +0000

    Working User class and test file (not unittest)

--- models/user.py
+++ models/user.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 """ User Class """
 from models.base_model import BaseModel
-from models import storage
 
 class User(BaseModel):
     """ User class inherits from BaseModel class """
-
-    def __init__(self, **kwargs):
-#        super.__init__(kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
